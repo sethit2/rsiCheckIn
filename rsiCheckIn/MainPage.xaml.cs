40ad5eb4d81f9a71b773004936989751e7e2cace
seth <sethrkaplanturner@gmail.com>
2023-12-02 15:09:11 -0600

    Add project files.

---
+++ rsiCheckIn/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using ZXing.Net.Maui;
+
+namespace rsiCheckIn
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            cameraBarcodeReaderView.Options = new BarcodeReaderOptions
+            {
+                Formats = BarcodeFormats.OneDimensional,
+                AutoRotate = true,
+                Multiple = true
+            };
+        }
+
+        protected void BarcodesDetected(object sender, BarcodeDetectionEventArgs e)
+        {
+            foreach (var barcode in e.Results)
+                Console.WriteLine($"Barcodes: {barcode.Format} -> {barcode.Value}");
+        }
+    }
+
+}
