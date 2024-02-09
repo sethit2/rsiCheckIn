a8348dc1169556a244f86ec1dd2299c2f457c218
Benji Altman <benjidaltman@gmail.com>
2024-02-09 11:04:19 -0600

    FUCK, the URL keeps changing

--- RsiApiWrapper/SpreadsheetConnector.cs
+++ RsiApiWrapper/SpreadsheetConnector.cs
@@ -23,11 +23,13 @@ namespace RsiApiWrapper
 		//	return JsonSerializer.Deserialize<Player[]>(payload);
 		//}
 
+		private const string URL = "https://script.google.com/macros/s/AKfycbyGCn4r_ObZO0f_UoaGSa6slm8lH8dz2RJTn9bOIo-QaXNjOfWDgebV5JghQ9eyiELd/exec";
+
 		public static async Task<bool> CheckinPlayers(IEnumerable<Guid> ids)
 		{
 			var payload = JsonSerializer.Serialize(ids);
 			using var httpClient = new HttpClient();
-			const string url = "https://script.google.com/macros/s/AKfycbxq8ZQK90D2iHHaWOcZSTERSdI6O0gaJEH8-1Y0w4lCDRQcsgx5zMuf2ZxvRZy9JOnd/exec?endpoint=checkin";
+			const string url = URL + "?endpoint=checkin";
 			var response = await httpClient.PostAsync(url, new StringContent(payload, Encoding.UTF8, "application/json"));
 			return response.StatusCode == System.Net.HttpStatusCode.OK
 				&& (await response.Content.ReadAsStringAsync()) == "1";
@@ -37,7 +39,7 @@ namespace RsiApiWrapper
 		{
 			var payload = JsonSerializer.Serialize(ids);
 			using var httpClient = new HttpClient();
-			const string url = "https://script.google.com/macros/s/AKfycbxq8ZQK90D2iHHaWOcZSTERSdI6O0gaJEH8-1Y0w4lCDRQcsgx5zMuf2ZxvRZy9JOnd/exec?endpoint=checkout";
+			const string url = URL + "?endpoint=checkout";
 			var response = await httpClient.PostAsync(url, new StringContent(payload, Encoding.UTF8, "application/json"));
 			return response.StatusCode == System.Net.HttpStatusCode.OK
 				&& (await response.Content.ReadAsStringAsync()) == "1";
